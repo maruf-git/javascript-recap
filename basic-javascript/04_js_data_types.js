d7da5b2ea2de31648fe03fa644eb04d4db184b7b
maruf-git <munna.csehstu@gmail.com>
2026-03-05 17:15:15 +0600

    data types and boolean

---
+++ basic-javascript/04_js_data_types.js
@@ -0,0 +1,33 @@
+// JavaScript has 8 Datatypes
+// A JavaScript variable can hold 8 types of data:
+// Type	Description
+// String	A text of characters enclosed in quotes
+// Number	A number representing a mathematical value
+// Bigint	A number representing a large integer
+// Boolean	A data type representing true or false
+// Object	A collection of key-value pairs of data
+// Undefined	A primitive variable with no assigned value
+// Null	A primitive value representing object absence
+// Symbol	A unique and primitive identifier
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
+console.log(x);
+console.log(y);
+
+// Object
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object
+const date = new Date("2022-03-25");
+
+console.log(date);
+console.log(typeof date);
+
+const check = Symbol();
+console.log(check);
+console.log(typeof check)
---
+++ basic-javascript/05_booleans.js
@@ -0,0 +1,8 @@
+// Everything Without a "Value" is False
+// Examples:
+// 0 is false
+// "" is false
+// undefined is false
+// null is false
+// NaN is false
+// false is false
